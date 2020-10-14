e84b78f36e5871640e2b4d11ff5b696ed60c5200
Richard Ingwe <richardingwe5@gmail.com>
2020-10-14 15:09:04 +0100

    worked on the phone number input

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const selectLga = document.querySelector("#lga");
 const selectCountry = document.querySelector("#country");
 selectCountry.length = 0;
 
+$(".tel").intlTelInput({
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
+});
 
 const data = async () => {
     const response = await axios.get('https://restcountries.eu/rest/v2/all');
@@ -12,7 +15,6 @@ const data = async () => {
 		console.log(response)
 		let option;
 		response.data.forEach((result, i) => {
-			console.log(response.data[i].name);
 			option = document.createElement("option");
 			option.text = response.data[i].name;
 			option.value = response.data[i].name;
